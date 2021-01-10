68fe8c76cbd3d641374de5f9c5f21e78c7be79e5
Frog-kt <kasuo.takumi@gmail.com>
2021-01-11 03:52:51 +0900

    chore : return type

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express';
 import authRouter from './auth';
 
-export function registRoutes(app: Express) {
+export function registRoutes(app: Express): void {
   app.use('/api/v1/auth', authRouter);
 }
