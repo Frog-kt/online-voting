c6d4708470b635ed0e99d491a9d2338bb5159410
Frog-kt <kasuo.takumi@gmail.com>
2021-01-11 22:51:42 +0900

    Add: signup

---
+++ src/controllers/authController.ts
@@ -0,0 +1,49 @@
+import { RequestHandler, Response } from 'express';
+import { prisma } from '@/prisma';
+import { hashPassword, matchPassword, sendTokenResponse } from '@/lib';
+import Joi from 'joi';
+
+import ErrorHandler from '../utils/ErrorHandler';
+import catchAsyncErrors from '../middlewares/catchAsyncErrors';
+import config from '../config';
+// import { ModelUser } from '@/db.types';
+
+interface NewAccountArgs {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const signup = catchAsyncErrors(async (req, res, next) => {
+  const signupSchema = Joi.object({
+    email: Joi.string().email().required(),
+    name: Joi.string().min(3).max(32).required(),
+    password: Joi.string().min(8).max(255).required(),
+  });
+
+  let request;
+  try {
+    request = await signupSchema.validateAsync(req.body);
+  } catch (err) {
+    return next(new ErrorHandler(err, 400));
+  }
+
+  const { name, email, password } = request as NewAccountArgs;
+
+  let saveUser;
+  try {
+    saveUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+  } catch (err) {
+    return next(new ErrorHandler(err, 400));
+  }
+
+  console.log(saveUser.id);
+
+  await sendTokenResponse(saveUser.id, res);
+});
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import { signup } from '../controllers/authController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.status(200).send('OK'); // テスト
-});
+router.post('/signup', signup);
 
 export default router;
