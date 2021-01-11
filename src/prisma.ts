1b1543ecced8de83fcd34f338e6d60b1bd8595d8
Frog-kt <kasuo.takumi@gmail.com>
2021-01-12 00:35:24 +0900

    Add: パスワード保存時にハッシュ化

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,2 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient();
+
+import { hashPassword } from '@/lib';
+
+// DBにパスワードを保存するときにデータをハッシュ化
+prisma.$use(async (params, next) => {
+  if (params.model === 'User') {
+    if (params.action === ('create' || 'update')) {
+      if (params.args.data.password) {
+        const hashedPassword = await hashPassword(params.args.data.password);
+        params.args.data.password = hashedPassword;
+      }
+    }
+  }
+  return next(params);
+});
