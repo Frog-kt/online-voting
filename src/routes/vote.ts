62e21a5034621314a82cad8e5f75920b4f0fbfb6
Frog-kt <kasuo.takumi@gmail.com>
2021-01-12 17:09:33 +0900

    Update vote.ts

--- src/routes/vote.ts
+++ src/routes/vote.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
-import { showVotes } from '../controllers/voteController';
+import multer from 'multer';
+import { showVotes, searchVoteTitle, searchVoteTag, postVotes, uploadImage } from '../controllers/voteController';
+import { requireAuth } from '../middlewares/auth';
 
+const upload = multer({ storage: multer.memoryStorage() });
 const router = Router();
 
 // 開催中の投票一覧取得
