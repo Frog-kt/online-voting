84c335e64bc247d317a76292cec0f68ab36d63cd
Frog-kt <kasuo.takumi@gmail.com>
2021-01-12 17:12:49 +0900

    Add: vote

--- src/controllers/voteController.ts
+++ src/controllers/voteController.ts
@@ -46,11 +46,26 @@ export const showVotes = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json(resObj);
 });
 
+// *タイトルで投票を検索する
+export const searchVoteTitle = catchAsyncErrors(async (req, res, next) => {
+  console.log(req.params);
 
-export const showVotes = catchAsyncErrors(async (req, res, next) => {
+  const word = req.params.title;
+
+  const val = await prisma.vote.findMany({
+    where: {
+      title: {
+        contains: word,
+      },
+    },
+  });
+
+  console.log(val);
+
   const resObj = {
     1: {
       id: '8',
+      endDatetime: '',
       title: '史上最強のエディター',
       imagePath: 'http://img.com/example.png',
       left: {
@@ -64,6 +79,7 @@ export const showVotes = catchAsyncErrors(async (req, res, next) => {
     },
     2: {
       id: '5',
+      endDatetime: '',
       title: 'きのこの山・たけのこの里',
       imagePath: 'http://img.com/example.png',
       left: {
@@ -78,4 +94,74 @@ export const showVotes = catchAsyncErrors(async (req, res, next) => {
   };
 
   res.status(200).json(resObj);
+});
+
+// *タグで投票を検索する
+export const searchVoteTag = catchAsyncErrors(async (req, res, next) => {
+  const resObj = {
+    1: {
+      id: '8',
+      endDatetime: '',
+      title: '史上最強のエディター',
+      imagePath: 'http://img.com/example.png',
+      left: {
+        title: 'Vim',
+        score: 20,
+      },
+      right: {
+        title: 'Emacs',
+        score: 30,
+      },
+    },
+    2: {
+      id: '5',
+      endDatetime: '',
+      title: 'きのこの山・たけのこの里',
+      imagePath: 'http://img.com/example.png',
+      left: {
+        title: 'Vim',
+        score: 20,
+      },
+      right: {
+        title: 'Emacs',
+        score: 30,
+      },
+    },
+  };
+
+  res.status(200).json(resObj);
+});
+
+// 新しい投票の作成
+export const postVotes = catchAsyncErrors(async (req, res, next) => {
+  res.status(200).send('OK');
+});
+
+// 新しい投票用の画像をアップロード
+export const uploadImage = catchAsyncErrors(async (req, res, next) => {
+  // const file = req.file;
+  // console.log(file);
+  // const imageId = uuid();
+
+  // if (!checkFileExt(file.mimetype)) {
+  //   return next(new ErrorHandler('Upload only accept jpeg, png, svg, gif and webp file.', 400));
+  // }
+
+  // try {
+  //   fs.writeFileSync(`./upload/${imageId}.jpg`, file.buffer, 'binary');
+  // } catch (err) {
+  //   console.log(err);
+  //   return next(new ErrorHandler('Upload failed.', 500));
+  // }
+  const tmp_path = req.file.path;
+  const target_path = 'upload/' + req.file.originalname;
+
+  console.log(tmp_path);
+  console.log(target_path);
+
+  const src = fs.createReadStream(tmp_path);
+  const dest = fs.createWriteStream(target_path);
+  src.pipe(dest);
+
+  res.status(200).send('ok');
 });
--- src/routes/vote.ts
+++ src/routes/vote.ts
@@ -7,5 +7,13 @@ const upload = multer({ storage: multer.memoryStorage() });
 const router = Router();
 
 router.get('/', showVotes); // 開催中の投票一覧取得
+router.post('/', requireAuth, postVotes); // 新しい投票を作成
+router.post('/image', requireAuth, upload.single('image'), uploadImage); // 画像のアップロード
+
+router.get('/search/title/:keyword', searchVoteTitle); // タイトルで検索
+router.get('/search/tag/:tagName', searchVoteTag); // タグで検索？
+
+router.get('/:voteId'); // 指定した投票を表示する
+router.post('/:voteId'); // 指定した投票に参加(投票)する
 
 export default router;
