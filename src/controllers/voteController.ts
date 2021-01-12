70fdd9bec5c0f0b33ef20ad731976cdcd17158e3
Frog-kt <kasuo.takumi@gmail.com>
2021-01-12 19:40:04 +0900

    Add: create vote

--- src/controllers/voteController.ts
+++ src/controllers/voteController.ts
@@ -10,6 +10,14 @@ import catchAsyncErrors from '../middlewares/catchAsyncErrors';
 import { sendJwtToken, getJwtToken } from '../utils/jwt';
 import config from '../config';
 
+interface NewVoteArgs {
+  title: string;
+  imagePath: string;
+  endDateTime: Date;
+  leftImageTitle: string;
+  rightImageTitle: string;
+}
+
 // *投票の一覧を表示 (人気順)
 export const showVotes = catchAsyncErrors(async (req, res, next) => {
   const resObj = {
@@ -134,20 +142,43 @@ export const searchVoteTag = catchAsyncErrors(async (req, res, next) => {
 
 // 新しい投票の作成
 export const postVotes = catchAsyncErrors(async (req, res, next) => {
-  res.status(200).send('OK');
+  const createVoteSchema = Joi.object({
+    title: Joi.string().min(5).max(32).required(),
+    imagePath: Joi.string().uri().required(),
+    endDateTime: Joi.date().timestamp(),
+    leftImageTitle: Joi.string().required(),
+    rightImageTitle: Joi.string().required(),
+  });
+
+  let request;
+  try {
+    request = await createVoteSchema.validateAsync(req.body);
+  } catch (err) {
+    return next(new ErrorHandler(err, 400));
+  }
+
+  console.log(request);
+
+  const { title, imagePath, endDateTime, leftImageTitle, rightImageTitle } = request as NewVoteArgs;
+
+  let saveVote;
+  try {
+    saveVote = await prisma.vote.create({
+      data: {
+        title,
+        imagePath,
+        leftImageTitle,
+        rightImageTitle,
+        endDateTime,
+      },
+    });
+  } catch (err) {
+    return next(new ErrorHandler(err, 401));
+  }
+
+  console.log(saveVote);
+
+  res.status(200).json(saveVote);
 });
 
-// 新しい投票用の画像をアップロード
-export const uploadImage = catchAsyncErrors(async (req, res, next) => {
-  const tmp_path = req.file.path;
-  const target_path = 'upload/' + req.file.originalname;
-
-  console.log(tmp_path);
-  console.log(target_path);
-
-  const src = fs.createReadStream(tmp_path);
-  const dest = fs.createWriteStream(target_path);
-  src.pipe(dest);
-
-  res.status(200).send('ok');
 });
